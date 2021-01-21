d3580b26d531be8d03a3a1d8c5d0100cebe0630a
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2021-01-21 14:28:48 +0100

    change

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'The latest commit SHA is: ' + os.environ.get('COMMIT_SHA', 'Not Found')
+    return 'The latest commit SHA is:  ' + os.environ.get('COMMIT_SHA', 'Not Found')
 
 if __name__ == "__main__":
     # Only for debugging while developing
